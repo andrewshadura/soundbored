72586f29202c0c1612c1e4b394e9a7485430323b
Tyler Tucker <tyler@mutual.app>
2021-11-12 18:01:24 -0700

    added link functionality

--- app/src/main/java/com/bri1/soundbored2/Credits.kt
+++ app/src/main/java/com/bri1/soundbored2/Credits.kt
@@ -2,10 +2,31 @@ package com.bri1.soundbored2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.TextView
 
 class Credits : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_credits)
+
+        setupHyperlink()
+    }
+
+    private fun setupHyperlink() {
+        val licenseLink = findViewById<TextView>(R.id.text_license_link)
+        licenseLink.setMovementMethod(LinkMovementMethod.getInstance())
+
+        val andrejLink = findViewById<TextView>(R.id.text_andrej)
+        andrejLink.setMovementMethod(LinkMovementMethod.getInstance())
+
+        val webcooltzLink = findViewById<TextView>(R.id.text_webcooltz)
+        webcooltzLink.setMovementMethod(LinkMovementMethod.getInstance())
+
+        val openSourceLink = findViewById<TextView>(R.id.text_open_source)
+        openSourceLink.setMovementMethod(LinkMovementMethod.getInstance())
+
+        val flatIconLink = findViewById<TextView>(R.id.text_icons)
+        flatIconLink.setMovementMethod(LinkMovementMethod.getInstance())
     }
 }
